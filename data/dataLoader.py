b5516707580289b91789467dd731ef3a68b03fdc
sumanabasu <sumana.basu21@gmail.com>
2018-05-03 16:17:16 -0400

    data loader module added

--- data/dataLoader.py
+++ data/dataLoader.py
@@ -8,6 +8,9 @@ from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from paths import paths, file_names
 import os
+import cPickle
+from paths import paths, file_names
+from modelConfig import params
 
 class HDF5loader():
 	def __init__(self, filename):
@@ -37,6 +40,27 @@ class HDF5loader():
 	def __len__(self):
 		return self.img_f.shape[0]
 	
+def dataLoader(hdf5_file):
+	data = HDF5loader(hdf5_file)
+	
+	train_indices = cPickle.load(open(os.path.join(paths['data']['Input_to_Training_Model'],
+												   file_names['data']['Train_set_indices']), 'r'))
+	
+	valid_indices = cPickle.load(open(os.path.join(paths['data']['Input_to_Training_Model'],
+												   file_names['data']['Valid_set_indices']), 'r'))
+	
+	test_indices = cPickle.load(open(os.path.join(paths['data']['Input_to_Training_Model'],
+												   file_names['data']['Test_set_indices']), 'r'))
+	
+	train_sampler = SubsetRandomSampler(train_indices)
+	valid_sampler = SubsetRandomSampler(valid_indices)
+	test_sampler = SubsetRandomSampler(test_indices)
+	
+	train_loader = DataLoader(data, batch_size=params['train']['batch_size'], sampler=train_sampler, num_workers=8)
+	valid_loader = DataLoader(data, batch_size=params['train']['batch_size'], sampler=valid_sampler, num_workers=8)
+	test_loader = DataLoader(data, batch_size=params['train']['batch_size'], sampler=test_sampler, num_workers=8)
+	
+	return (train_loader, valid_loader, test_loader)
 	
 def run_tests():
 	n_gpus = 1
