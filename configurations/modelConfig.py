ac6857de4b45bb0e9413ca974e70dcd6576b59b1
sumanabasu <sumana.basu21@gmail.com>
2018-05-22 13:44:30 -0400

    conv5 dropped, kernel=5, stride increased

--- configurations/modelConfig.py
+++ configurations/modelConfig.py
@@ -2,44 +2,36 @@ layer_config = {
 	'conv1': {
 		'in_channels': 1,
 		'out_channels': 16,
-		'kernel_size': 3,
+		'kernel_size': 5,
 		'stride': 1,
 		'padding': 1
 	},
 	'conv2': {
 		'in_channels': 16,
 		'out_channels': 32,
-		'kernel_size': 3,
-		'stride': 2,
+		'kernel_size': 5,
+		'stride': 3,
 		'padding': 0,
 		'dilation': 1
 	},
 	'conv3': {
 		'in_channels': 32,
 		'out_channels': 64,
-		'kernel_size': 3,
-		'stride': 2,
+		'kernel_size': 5,
+		'stride': 4,
 		'padding': 0,
 		'dilation': 1
 	},
 	'conv4': {
 		'in_channels': 64,
-		'out_channels': 128,
-		'kernel_size': 3,
-		'stride': 2,
-		'padding': 0,
-		'dilation': 1
-	},
-	'conv5': {
-		'in_channels': 128,
-		'out_channels': 128,
-		'kernel_size': 3,
-		'stride': 2,
+		'out_channels': 64,
+		'kernel_size': 5,
+		'stride': 4,
 		'padding': 0,
 		'dilation': 1
 	},
 	'fc1'	:	{
-		'in'	:	128 * 13 * 11 * 10,
+		'in'	:	64 * 4 * 3 * 3,
 		'out'	:	1024
 	},
 	'fc2'	:	{
@@ -65,15 +57,15 @@ params	=	{
 	'train'	:	{
 		'learning_rate' 	: 0.0001,
 		'num_epochs' 		: 100,
-		'batch_size' 		: 3,
+		'batch_size' 		: 4,
 		'label_weights' 	: [0.3, 0.75, 1]
 	}
 }
 
 # data augmentation
 data_aug = {
-	'horizontal_flip': 0.5,
-	'vertical_flip': 0.5,
+	#'horizontal_flip': 0.5,
+	#'vertical_flip': 0.5,
 	#'spline_warp': True,
 	#'warp_sigma': 0.1,
 	#'warp_grid_size': 3,
