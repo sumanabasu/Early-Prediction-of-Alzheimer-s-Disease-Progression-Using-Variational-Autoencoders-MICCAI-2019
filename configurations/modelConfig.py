891eb78215bc31b2646d7820b04a9639fd6ee058
sumanabasu <sumana.basu21@gmail.com>
2018-05-24 16:44:57 -0400

    maxpool3d, adaptivemaxpool3d, single fc layer, kernels 7, 5, 3, 3

--- configurations/modelConfig.py
+++ configurations/modelConfig.py
@@ -2,62 +2,63 @@ layer_config = {
 	'conv1': {
 		'in_channels': 1,
 		'out_channels': 16,
-		'kernel_size': 5,
+		'kernel_size': 7,
 		'stride': 1,
-		'padding': 1
+		'padding': 0
 	},
 	'conv2': {
 		'in_channels': 16,
 		'out_channels': 32,
 		'kernel_size': 5,
-		'stride': 3,
-		'padding': 0,
-		'dilation': 1
+		'stride': 1,
+		'padding': 0
 	},
 	'conv3': {
 		'in_channels': 32,
 		'out_channels': 64,
-		'kernel_size': 5,
-		'stride': 4,
-		'padding': 0,
-		'dilation': 1
+		'kernel_size': 3,
+		'stride': 1,
+		'padding': 0
 	},
 	'conv4': {
 		'in_channels': 64,
-		'out_channels': 64,
-		'kernel_size': 5,
-		'stride': 4,
-		'padding': 0,
-		'dilation': 1
+		'out_channels': 128,
+		'kernel_size': 3,
+		'stride': 1,
+		'padding': 0
 	},
 	'fc1'	:	{
-		'in'	:	64 * 4 * 3 * 3,
-		'out'	:	1024
-	},
-	'fc2'	:	{
-		'in'	: 	1024,
-		'out'	: 	512
-	},
-	'fc3'	:	{
-		'in'	:	512,
-		'out'	: 	256
-	},
-	'fc4'	:	{
-		'in'	: 	256,
+		'in'	: 	128,
 		'out'	:	3
+	},
+	
+	'maxpool3d'	:	{
+		'layer1'	:	{
+			'kernel'	:	7,
+			'stride'		:	3
+		},
+		'layer2'	:	{
+			'kernel'	:	5,
+			'stride'		:	3
+		},
+		'layer3'	:	{
+			'kernel'	:	3,
+			'stride'		:	2
+		},
+		'adaptive'	:	1
 	}
 }
 
 params	=	{
 	'model'	:	{
-		'conv_drop_prob'	: 0.5,
-		'fcc_drop_prob'		: 0.5
+		'conv_drop_prob'	: 0.2,
+		'fcc_drop_prob'		: 0.2
 	},
 	
 	'train'	:	{
-		'learning_rate' 	: 0.0005,
+		'learning_rate' 	: 0.0001,
 		'num_epochs' 		: 100,
-		'batch_size' 		: 4,
+		'batch_size' 		: 8,
 		'label_weights' 	: [0.3, 0.75, 1]
 	}
 }
