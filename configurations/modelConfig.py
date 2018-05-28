49ee231bcbb7300bcdfafb5348774edf71b11dc4
sumanabasu <sumana.basu21@gmail.com>
2018-05-28 16:38:25 -0400

    adaptivemaxpool3d

--- configurations/modelConfig.py
+++ configurations/modelConfig.py
@@ -1,47 +1,66 @@
 layer_config = {
 	'conv1': {
 		'in_channels': 1,
-		'out_channels': 16,
+		'out_channels': 32,
 		'kernel_size': 7,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv2': {
-		'in_channels': 16,
-		'out_channels': 32,
+		'in_channels': 32,
+		'out_channels': 64,
 		'kernel_size': 5,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv3': {
-		'in_channels': 32,
-		'out_channels': 64,
-		'kernel_size': 3,
+		'in_channels': 64,
+		'out_channels': 128,
+		'kernel_size': 5,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv4': {
-		'in_channels': 64,
-		'out_channels': 128,
+		'in_channels': 128,
+		'out_channels': 256,
 		'kernel_size': 3,
 		'stride': 1,
 		'padding': 0
 	},
-	'fc1'	:	{
-		'in'	: 	128,
-		'out'	:	3
+	'conv5': {
+		'in_channels': 256,
+		'out_channels': 512,
+		'kernel_size': 3,
+		'stride': 1,
+		'padding': 0
+	},
+	'conv6': {
+		'in_channels': 512,
+		'out_channels': 1024,
+		'kernel_size': 3,
+		'stride': 1,
+		'padding': 0
 	},
 	
+	'fc1'	:	{
+		'in'	: 	512,
+		'out'	:	512
+	},
+	'fc2'	:	{
+		'in'	: 	512,
+		'out'	:	3
+		},
+		
 	'maxpool3d'	:	{
-		'layer1'	:	{
+		'k7'	:	{
 			'kernel'	:	7,
 			'stride'		:	3
 		},
-		'layer2'	:	{
+		'k5'	:	{
 			'kernel'	:	5,
-			'stride'		:	3
+			'stride'		:	2
 		},
-		'layer3'	:	{
+		'k3'	:	{
 			'kernel'	:	3,
 			'stride'		:	2
 		},
@@ -52,13 +71,13 @@ layer_config = {
 params	=	{
 	'model'	:	{
 		'conv_drop_prob'	: 0.2,
-		'fcc_drop_prob'		: 0.2
+		'fcc_drop_prob'		: 0.0
 	},
 	
 	'train'	:	{
 		'learning_rate' 	: 0.0001,
 		'num_epochs' 		: 100,
-		'batch_size' 		: 8,
+		'batch_size' 		: 1,
 		'label_weights' 	: [0.3, 0.75, 1]
 	}
 }
--- models/cnn.py
+++ models/cnn.py
@@ -21,24 +21,35 @@ class CnnVanilla(nn.Module):
 		self.conv4 = nn.Conv3d(in_channels=layer_config['conv4']['in_channels'], out_channels=layer_config['conv4']['out_channels'],
 							   kernel_size=layer_config['conv4']['kernel_size'], stride=layer_config['conv4']['stride'],
 							   padding=layer_config['conv4']['padding'])
+		self.conv5 = nn.Conv3d(in_channels=layer_config['conv5']['in_channels'],
+							   out_channels=layer_config['conv5']['out_channels'],
+							   kernel_size=layer_config['conv5']['kernel_size'], stride=layer_config['conv5']['stride'],
+							   padding=layer_config['conv5']['padding'])
+		self.conv6 = nn.Conv3d(in_channels=layer_config['conv6']['in_channels'],
+							   out_channels=layer_config['conv6']['out_channels'],
+							   kernel_size=layer_config['conv6']['kernel_size'], stride=layer_config['conv6']['stride'],
+							   padding=layer_config['conv6']['padding'])
 	
 		
 		self.fc1 = nn.Linear(layer_config['fc1']['in'] , layer_config['fc1']['out'])
+		self.fc2 = nn.Linear(layer_config['fc2']['in'], layer_config['fc2']['out'])
 		
 		self.bn1 = nn.BatchNorm3d(layer_config['conv1']['out_channels'])
 		self.bn2 = nn.BatchNorm3d(layer_config['conv2']['out_channels'])
 		self.bn3 = nn.BatchNorm3d(layer_config['conv3']['out_channels'])
 		self.bn4 = nn.BatchNorm3d(layer_config['conv4']['out_channels'])
+		self.bn5 = nn.BatchNorm3d(layer_config['conv5']['out_channels'])
+		self.bn6 = nn.BatchNorm3d(layer_config['conv6']['out_channels'])
 		
 		self.dropout3d = nn.Dropout3d(p=params['model']['conv_drop_prob'])
 		self.dropout = nn.Dropout(params['model']['fcc_drop_prob'])
 		
-		self.mp3dL1 = nn.MaxPool3d(layer_config['maxpool3d']['layer1']['kernel'], layer_config['maxpool3d'][
-			'layer1']['stride'])
-		self.mp3dL2 = nn.MaxPool3d(layer_config['maxpool3d']['layer2']['kernel'], layer_config['maxpool3d'][
-			'layer2']['stride'])
-		self.mp3dL3 = nn.MaxPool3d(layer_config['maxpool3d']['layer3']['kernel'], layer_config['maxpool3d'][
-			'layer3']['stride'])
+		self.k7 = nn.MaxPool3d(layer_config['maxpool3d']['k7']['kernel'], layer_config['maxpool3d'][
+			'k7']['stride'])
+		self.k5 = nn.MaxPool3d(layer_config['maxpool3d']['k5']['kernel'], layer_config['maxpool3d'][
+			'k5']['stride'])
+		self.k3 = nn.MaxPool3d(layer_config['maxpool3d']['k3']['kernel'], layer_config['maxpool3d'][
+			'k3']['stride'])
 		self.adaptiveMp3d = nn.AdaptiveMaxPool3d(layer_config['maxpool3d']['adaptive'])
 		
 		self.relu = nn.ReLU()
@@ -63,19 +74,31 @@ class CnnVanilla(nn.Module):
 	
 	def forward(self, x):
 		# print(x.size())
-		out16 = self.relu(self.bn1(self.mp3dL1(self.conv1(x))))
-		out16 = self.dropout3d(out16)
-		# print(out16.size())
-		out32 = self.relu(self.bn2(self.mp3dL2(self.conv2(out16))))
-		out32 = self.dropout3d(out32)
-		# print(out32.size())
-		out64 = self.relu(self.bn3(self.mp3dL3(self.conv3(out32))))
-		out64 = self.dropout3d(out64)
-		# print(out64.size())
-		out128 = self.relu(self.bn4(self.adaptiveMp3d(self.conv4(out64))))
-		out128 = self.dropout3d(out128)
-		# print(out128.size())
-		flat = out128.view(out128.size(0), -1)
-		# print(flat.size())
-		fcc = self.logsoftmax(self.fc1(flat))
+		out32 = self.dropout3d(self.k7(self.relu(self.bn1(self.conv1(x)))))
+		#print(out32.size())
+		
+		out64 = self.dropout3d(self.k5(self.relu(self.bn2(self.conv2(out32)))))
+		#print(out64.size())
+		
+		out128 = self.dropout3d(self.k5(self.relu(self.bn3(self.conv3(out64)))))
+		#print(out128.size())
+		
+		out256 = self.dropout3d(self.k3(self.relu(self.bn4(self.conv4(out128)))))
+		# print(out256.size())
+		
+		
+		out512 = self.dropout3d(self.adaptiveMp3d(self.relu(self.bn5(self.conv5(out256)))))
+		#print(out512.size())
+		'''
+		out1024 = self.dropout3d(self.adaptiveMp3d(self.relu(self.bn6(self.conv6(out512)))))
+		print(out1024.size())
+		'''
+		
+		flat = out512.view(out512.size(0), -1)
+		#print(flat.size())
+		
+		fcc1 = self.dropout(self.relu(self.fc1(flat)))
+		#print(fcc1.size())
+		
+		fcc = self.logsoftmax(self.fc2(fcc1))
 		return fcc
