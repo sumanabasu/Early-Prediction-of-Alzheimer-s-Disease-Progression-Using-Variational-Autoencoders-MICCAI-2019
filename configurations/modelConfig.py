4d76e70f3f55e6289dc9da4724d6800d5fb0b5b2
sumanabasu <sumana.basu21@gmail.com>
2018-06-05 15:32:57 -0400

    automatic dimension calculation for FC layer

--- configurations/modelConfig.py
+++ configurations/modelConfig.py
@@ -1,44 +1,59 @@
+import numpy as np
+
 num_classes = 2
+
+
+num_conv = 4
+img_shape = np.array([213, 197, 189])
+
+for i in range(3):
+	for _ in range(num_conv):
+		if i == 0 and _ == 0:
+			img_shape[0] = img_shape[0] - 2
+		else:
+			img_shape[i] /= 2
+			img_shape[i]-= 1
+
 
 layer_config = {
 	'conv1': {
 		'in_channels': 1,
-		'out_channels': 5,
+		'out_channels': 9,
 		'kernel_size': 3,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv2': {
-		'in_channels': 5,
-		'out_channels': 5,
+		'in_channels': 9,
+		'out_channels': 9,
 		'kernel_size': 3,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv3': {
-		'in_channels': 5,
-		'out_channels': 5,
+		'in_channels': 9,
+		'out_channels': 11,
 		'kernel_size': 3,
 		'stride': 1,
 		'padding': 0
 	},
 	'conv4': {
-		'in_channels': 5,
-		'out_channels': 5,
+		'in_channels': 11,
+		'out_channels': 11,
 		'kernel_size': 3,
 		'stride': 1,
 		'padding': 0
 	},
 	
 	'fc1'	:	{
-		'in'	: 	5 * 24 * 10 * 9,
+		'in'	: 	11 * img_shape[0] * img_shape[1] * img_shape[2],
 		'out'	:	4096
 	},
 	'fc2'	:	{
 		'in'	: 	4096,
 		'out'	:	num_classes
-		},
-		
+	},
+	
 	'maxpool3d'	:	{
 		'l1'	:	{						#to preserve temporal information in  the early phase
 			'kernel'	:	(1, 2, 2),
@@ -62,7 +77,7 @@ params	=	{
 	'train'	:	{
 		'learning_rate' 	: 0.0001,
 		'num_epochs' 		: 100,
-		'batch_size' 		: 8,
+		'batch_size' 		: 6,
 		'label_weights' 	: [1, 1]	#[0.3, 0.75, 1]
 	}
 }
@@ -71,9 +86,9 @@ params	=	{
 data_aug = {
 	'horizontal_flip': 0.5,
 	'vertical_flip': 0.5,
-	'spline_warp': True,
-	'warp_sigma': 0.1,
-	'warp_grid_size': 3,
+	#'spline_warp': True,
+	#'warp_sigma': 0.1,
+	#'warp_grid_size': 3,
 	## 'crop_size': (100, 100),
-	'channel_shift_range': 5.
+	#'channel_shift_range': 5.
 }
