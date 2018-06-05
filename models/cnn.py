69738f976a0127f9f4ba7a3d42d5bd80e3ae4d1d
sumanabasu <sumana.basu21@gmail.com>
2018-06-05 15:33:47 -0400

    conv5, FC2 removed

--- models/cnn.py
+++ models/cnn.py
@@ -21,15 +21,22 @@ class CnnVanilla(nn.Module):
 		self.conv4 = nn.Conv3d(in_channels=layer_config['conv4']['in_channels'], out_channels=layer_config['conv4']['out_channels'],
 							   kernel_size=layer_config['conv4']['kernel_size'], stride=layer_config['conv4']['stride'],
 							   padding=layer_config['conv4']['padding'])
-	
+		'''
+		self.conv5 = nn.Conv3d(in_channels=layer_config['conv5']['in_channels'],
+							   out_channels=layer_config['conv5']['out_channels'],
+							   kernel_size=layer_config['conv5']['kernel_size'], stride=layer_config['conv5']['stride'],
+							   padding=layer_config['conv5']['padding'])
+		'''
 		
 		self.fc1 = nn.Linear(layer_config['fc1']['in'] , layer_config['fc1']['out'])
 		self.fc2 = nn.Linear(layer_config['fc2']['in'], layer_config['fc2']['out'])
+		#self.fc3 = nn.Linear(layer_config['fc3']['in'], layer_config['fc3']['out'])
 		
 		self.bn1 = nn.BatchNorm3d(layer_config['conv1']['out_channels'])
 		self.bn2 = nn.BatchNorm3d(layer_config['conv2']['out_channels'])
 		self.bn3 = nn.BatchNorm3d(layer_config['conv3']['out_channels'])
 		self.bn4 = nn.BatchNorm3d(layer_config['conv4']['out_channels'])
+		# self.bn5 = nn.BatchNorm3d(layer_config['conv5']['out_channels'])
 		
 		self.dropout3d = nn.Dropout3d(p=params['model']['conv_drop_prob'])
 		self.dropout = nn.Dropout(params['model']['fcc_drop_prob'])
@@ -62,7 +69,8 @@ class CnnVanilla(nn.Module):
 				nn.init.constant(m.bias.data, 0.01)
 	
 	def forward(self, x):
-		# print(x.size())
+		#print(x.size())
+		
 		out1 = self.dropout3d(self.maxpool1(self.relu(self.bn1(self.conv1(x)))))
 		#print(out1.size())
 		
@@ -75,11 +83,17 @@ class CnnVanilla(nn.Module):
 		out4 = self.dropout3d(self.maxpool(self.relu(self.bn4(self.conv4(out3)))))
 		#print(out4.size())
 		
+		#out5 = self.dropout3d(self.maxpool(self.relu(self.bn4(self.conv4(out4)))))
+		#print(out5.size())
+		
 		flat = out4.view(out4.size(0), -1)
 		#print(flat.size())
 		
 		fcc1 = self.dropout(self.relu(self.fc1(flat)))
 		#print(fcc1.size())
+		
+		# output = self.dropout(self.relu(self.fc2(fcc1)))
+		# print(fcc2.size())
 		
 		output = self.logsoftmax(self.fc2(fcc1))
 		#print(output.size())
